4862dfd0572d36add5bc37e4a5d2646ac7b77f6f
xiebohh <xiebo@hh-medic.com>
2016-08-08 18:51:58 +0800

    Hello World!

---
+++ 001.SecKill/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by xiebo on 2016-8-8.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    private void test() {
+
+    }
+}
